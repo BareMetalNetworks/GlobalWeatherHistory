6dabf4a39e82a8230b994956ddd5ffdd8e82f9e1
S K <sam@baremetalnetworks.com>
2021-11-11 06:35:14 -0700

    added get forecast

--- main.rb
+++ main.rb
@@ -2,48 +2,46 @@ require 'csv'
 require 'sqlite3'
 require 'active_record'
 require 'pry'
+require_relative 'libdb'
 
 # Connect to an in-memory sqlite3 database
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
-  database: ':memory:'
+  database: './database'
 )
 
-ActiveRecord::Schema.define do
-    create_table :joslin_fields, force: true do |t|
-      t.date :capture_date
-      t.float :temp
-      t.float :dew_point
-      t.float :station_pressure
-      t.float :sea_level_pressure
-      t.float :visibility
-      t.float :wind_speed
-      t.float :wind_max
-     # t.float :wind_gust
-      t.float :temp_max
-      t.float :temp_min
-      t.float :precipitation
-      t.float :snow_depth
-
-    end
-  end
-
-  class JoslinField < ActiveRecord::Base; end
-
-data = []
-
-CSV.foreach("72586699999.csv") do |row|
-row.map{ |x| x.gsub(/[^0-9a-z ]/i, '')}
-data.push row
-entry = JoslinField.create!(capture_date: row[1], temp: row[6], dew_point: row[8],
-    visibility: row[14], wind_speed: row[16], wind_max: row[18], #wind_gust: row[19], 
-    temp_max: row[20], temp_min: row[22], precipitation: row[24], snow_depth: row[26],
-    station_pressure: row[12], sea_level_pressure: row[10])
-
-end
-
-p JoslinField.find_by(capture_date: "1995-7-22")
-
+p History.all.count
 
 __END__
-
+# CSV file meta
+#_____________________________
+# Index | Heading | DB_field_name
+#-----------------------------
+# 0 "STATION" station
+# 1 "DATE" capture_date
+# 2 "LATITUDE" lat
+# 3 "LONGITUDE" long
+# 4 "ELEVATION" elevation
+# 5 "NAME" location
+# 6 "TEMP" temp_avg
+# 7 "TEMP_ATTRIBUTES"
+# 8 "DEWP" dewpoint 
+# 9 "DEWP_ATTRIBUTES"
+# 10 "SLP" sea_level_pressure 
+# 11 "SLP_ATTRIBUTES"
+# 12 "STP" station_pressure
+# 13 "STP_ATTRIBUTES"
+# 14 "VISIB" Visibility
+# 15 "VISIB_ATTRIBUTES"
+# 16 "WDSP" wind_speed
+# 17 "WDSP_ATTRIBUTES"
+# 18 "MXSPD" wind_max
+# 19 "GUST" wind_gust
+# 20 "MAX" temp_max
+# 21 "MAX_ATTRIBUTES"
+# 22 "MIN" temp_min
+# 23 "MIN_ATTRIBUTES"
+# 24 "PRCP" precipitation
+# 25 "PRCP_ATTRIBUTES"
+# 26 "SNDP" snow_depth
+# 27 "FRSHTT"
