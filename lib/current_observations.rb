4ea917d512f43057b0019d5e27000ca3bf2a8e3f
safeaf <transiencymail@gmail.com>
2021-12-07 09:55:08 -0700

    cleaned up and added missing instance vars

--- lib/current_observations.rb
+++ lib/current_observations.rb
@@ -2,32 +2,34 @@ require 'httparty'
 require 'json'
 
 class CurrentObservations
+  attr_reader :current, :forecast, :alerts
   def initialize(station_id, station, state)
     @station_id = station_id # reduce to one id
     @station = station
     @state = state
     @response = []
+    @current = []
+    @forecast = []
+    @alerts = []
   end
 
   def request(url)
-    resp = HTTParty.get(url)
-    resp.body if resp.code == 200
-    @response = JSON.parse(resp.body)
+    @response = JSON.parse(HTTParty.get(url).body)
   end
 
   def get_current
-    request("https://api.weather.gov/stations/#{@station_id}/observations")
+    @current = request("https://api.weather.gov/stations/#{@station_id}/observations")
   end
 
   def get_forecast
-    request("https://api.weather.gov/gridpoints/#{@station}/forecast")
+    @forecast = request("https://api.weather.gov/gridpoints/#{@station}/forecast")
   end
   def get_alerts
-    request("https://api.weather.gov/alerts/active?area=#{@state}")
+    @alerts = request("https://api.weather.gov/alerts/active?area=#{@state}")
   end
 end
 
 
-#c = CurrentObservations.new("KTWF", 'BOI/182,24', "ID")
-#p c.get_current
+c = CurrentObservations.new("KTWF", 'BOI/182,24', "ID")
+p c.get_current
 #p c.get_forecast
