9011042379758f20882eda50309ce35a48e1f904
safeaf <transiencymail@gmail.com>
2021-12-12 09:58:00 -0700

    rewriting observation object creation

--- lib/current_observations.rb
+++ lib/current_observations.rb
@@ -9,16 +9,28 @@ ActiveRecord::Base.establish_connection(
 
 
 ActiveRecord::Schema.define do
+  create_table :stations, force: true do |t|
+    t.string :location
+    t.float :lat
+    t.float :long
+    t.float :elevation
+    t.string :name
+    t.string :station_id
+    t.string :state
+    t.string :station_name
+end
+
     create_table :observations, force: true do |t|
       t.integer :epoch
       #t.string :location
       t.date :capture
+      t.string :raw_date
       #t.float :lat
       #t.float :long
       #t.float :elevation
       #t.float :temp_avg
       t.float :dew_point
-      t.float :temeprature
+      t.float :temperature
       t.float :station_pressure
       t.float :sea_level_pressure
       t.float :visibility
@@ -36,13 +48,7 @@ ActiveRecord::Schema.define do
       t.belongs_to :station, index: true
     end
 
-      create_table :stations, force: true do |t|
-        t.string :location
-        t.float :lat
-        t.float :long
-        t.float :elevation
-        t.string :name
-    end
+
   end
 
 class Station < ActiveRecord::Base
@@ -51,46 +57,22 @@ end
 
 class Observation < ActiveRecord::Base
   belongs_to :station
-end
 
-class Foo
-  def initialize()
-  #   @key = create_primary_key(p["timestamp"])
-  # #  @obs = p
-  #   @timestamp = p["timestamp"]
-  #   @temperature = convert_c_to_f(p["temperature"]["value"])
-  #   @windDirection = wind_direction_nil_guard(p["windDirection"]["value"])
-  #   @windSpeed = p["windSpeed"]["value"] || 0.0
-  #   @windGust = wind_gust_nil_guard(p["windGust"]["value"])
-  #   @pressure = p["barometricPressure"]["value"]
-  #   @seaLevelPressure = p["seaLevelPressure"]["value"]
-  #   @visibility = p["visibility"]["value"]
-  # #  @presentWeather = p["presentWeather"]
-  #   @dewPoint = dewpoint_nil_guard(p["dewPoint"])
-  #   @maxTemp = p["maxTemperatureLast24Hours"]["value"]
-  #   @minTemp = p["minTemperatureLast24Hours"]["value"]
-  #   @precipLastHour = precip_nil_guard(p["preciptationLastHour"])
-  # #  @precipLast3Hours = p["preciptationLast3Hours"]
-  # #  @precipLast6Hours = p["preciptationLast6Hours"]
-  # #  @humidity = p["realtiveHumidity"]
-  #   @windChill = truncate_wind_chill(p["windChill"]["value"])
-  # #  @heatIndex = p["heatIndex"]["value"]
-  #   @cloudLayers = get_cloud_layers(p["cloudLayers"])
-  end
+  #
+  # def get_cloud_layers(l)
+  #   return "UNK" if l[0].nil?
+  #   l[0]["amount"]
+  # end
+  #
+  #   def create_primary_key(raw_timestamp) # 2021-12-09T03:53:00+00:00
+  #     b = DateTime.parse(raw_timestamp)
+  #     Time.new(b.year, b.month, b.day, b.hour, b.minute).to_i
+  #   end
+  #
+  #   def convert_c_to_f(temp)
+  #     (temp.to_f * 9.0 / 5.0 + 32).truncate(2)
+  #   end
 
-def get_cloud_layers(l)
-  return "UNK" if l[0].nil?
-  l[0]["amount"]
-end
-
-  def create_primary_key(raw_timestamp) # 2021-12-09T03:53:00+00:00
-    b = DateTime.parse(raw_timestamp)
-    Time.new(b.year, b.month, b.day, b.hour, b.minute).to_i
-  end
-
-  def convert_c_to_f(temp)
-    (temp.to_f * 9.0 / 5.0 + 32).truncate(2)
-  end
 end
 
 class CurrentObservations
@@ -101,7 +83,7 @@ class CurrentObservations
     @state = state
     @response = []
     @raw_current = []
-    @current = [] #AoH
+    @current =[]
     @forecast = []
     @alerts = []
   end
@@ -115,7 +97,7 @@ class CurrentObservations
       request("https://api.weather.gov/stations/#{@station_id}/observations")
 
     @raw_current["features"].each do |c|
-      @current.push(Observation.create!(c["properties"]))
+      @current.push(c["properties"])
     end
     #@current
   end
@@ -133,11 +115,56 @@ end
 s = Station.create(name: "Foo")
 p s
 
-o = Observation.create
-p o
+#   @key = create_primary_key(p["timestamp"])
+# #  @obs = p
+#   @timestamp = p["timestamp"]
+#   @temperature = convert_c_to_f(p["temperature"]["value"])
+#   @windDirection = wind_direction_nil_guard(p["windDirection"]["value"])
+#   @windSpeed = p["windSpeed"]["value"] || 0.0
+#   @windGust = wind_gust_nil_guard(p["windGust"]["value"])
+#   @pressure = p["barometricPressure"]["value"]
+#   @seaLevelPressure = p["seaLevelPressure"]["value"]
+#   @visibility = p["visibility"]["value"]
+# #  @presentWeather = p["presentWeather"]
+#   @dewPoint = dewpoint_nil_guard(p["dewPoint"])
+#   @maxTemp = p["maxTemperatureLast24Hours"]["value"]
+#   @minTemp = p["minTemperatureLast24Hours"]["value"]
+#   @precipLastHour = precip_nil_guard(p["preciptationLastHour"])
+# #  @precipLast3Hours = p["preciptationLast3Hours"]
+# #  @precipLast6Hours = p["preciptationLast6Hours"]
+# #  @humidity = p["realtiveHumidity"]
+#   @windChill = truncate_wind_chill(p["windChill"]["value"])
+# #  @heatIndex = p["heatIndex"]["value"]
+#   @cloudLayers = get_cloud_layers(p["cloudLayers"])
 
-s.observations << o
-p s.observations
+ c = CurrentObservations.new("KTWF", 'BOI/182,24', "ID")
+  c.get_current
+
+ c.current.each do |p|
+   p p["timestamp"]
+  #  o = Observation.create!(raw_date: p["timestamp"] )
+#    s.observations << o
+end
+
+ #p s.observations
+
+# t.float :dew_point
+# t.float :temeprature
+# t.float :station_pressure
+# t.float :sea_level_pressure
+# t.float :visibility
+# t.float :wind_speed
+# t.float :wind_gust
+# t.float :max_temp
+# t.float :min_temp
+# t.float :precipitation
+# t.float :heat_index
+# t.string :cloud_layers
+# t.float :humidity
+# t.float :wind_chill
+# t.float :wind_direction
+# s.observations << o
+# p s.observations
 
 # c = CurrentObservations.new("KTWF", 'BOI/182,24', "ID")
 #  c.get_current
